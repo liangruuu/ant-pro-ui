ea78b6f4ab58994473096708fa657a6ca8c98851
Dazzle Chen <306273815@qq.com>
2020-07-16 19:53:02 +0800

    <fix> fix add ent

--- src/models/cd_industry.ts
+++ src/models/cd_industry.ts
@@ -2,10 +2,11 @@ import { Effect } from 'dva';
 import { Reducer } from 'react';
 import { message } from 'antd';
 import { TreeNode } from './entity';
-import { fetchCdIndustry } from '@/services/cd_industry';
+import { fetchCdIndustry, fetchSuperviseTypeTree } from '@/services/cd_industry';
 
 export interface CdIndustryModelState {
   cdIndustryTree?: TreeNode[];
+  cdSuperviseTypeTree?: TreeNode[];
 }
 
 export interface CdIndustryModelType {
@@ -13,6 +14,7 @@ export interface CdIndustryModelType {
   state: CdIndustryModelState;
   effects: {
     fetchCdIndustry: Effect;
+    fetchSuperviseTypeTree: Effect;
   };
   reducers: {
     save: Reducer<any, any>;
@@ -32,6 +34,20 @@ const CdIndustry: CdIndustryModelType = {
             type: 'save',
             payload: res.data,
             index: 'cdIndustryTree',
+          });
+        }
+      } catch (e) {
+        message.error(e || '未知错误');
+      }
+    },
+    *fetchSuperviseTypeTree({ payload }, { call, put }) {
+      try {
+        const res = yield call(fetchSuperviseTypeTree, payload);
+        if (res.code === 200) {
+          yield put({
+            type: 'save',
+            payload: res.data,
+            index: 'cdSuperviseTypeTree',
           });
         }
       } catch (e) {
