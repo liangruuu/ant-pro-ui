cd31875910ff300d7db419fe398e111501a6cfbb
Dazzle Chen <306273815@qq.com>
2020-07-16 18:49:35 +0800

    <fix> user login status fix

--- src/layouts/SecurityLayout.tsx
+++ src/layouts/SecurityLayout.tsx
@@ -5,6 +5,7 @@ import { Redirect } from 'umi';
 import { stringify } from 'querystring';
 import { ConnectState, ConnectProps } from '@/models/connect';
 import { CurrentUser } from '@/models/user';
+import { getAuthority } from '@/utils/authority';
 
 interface SecurityLayoutProps extends ConnectProps {
   loading?: boolean;
@@ -34,10 +35,10 @@ class SecurityLayout extends React.Component<SecurityLayoutProps, SecurityLayout
 
   render() {
     const { isReady } = this.state;
-    const { children, loading, currentUser } = this.props;
+    const { children, loading } = this.props;
     // You can replace it to your authentication rule (such as check token exists)
     // 你可以把它替换成你自己的登录认证规则（比如判断 token 是否存在）
-    const isLogin = currentUser && currentUser.userid;
+    const isLogin = getAuthority() && getAuthority().toString();
     const queryString = stringify({
       redirect: window.location.href,
     });
